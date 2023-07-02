ebe2981cce7c86e66cb4fb20b8784bbcb4b82158
Mateusz <mateusz.laczny@interia.pl>
2023-07-02 17:35:24 +0200

    fix: Fixed BpmnGatewayType serialization

--- java-backend/src/main/java/edu/agh/bpmnai/generator/bpmn/model/BpmnGatewayType.java
+++ java-backend/src/main/java/edu/agh/bpmnai/generator/bpmn/model/BpmnGatewayType.java
@@ -1,6 +1,19 @@
 package edu.agh.bpmnai.generator.bpmn.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum BpmnGatewayType {
-    EXCLUSIVE,
-    INCLUSIVE
+    EXCLUSIVE("exclusive"),
+    INCLUSIVE("inclusive");
+
+    private final String jsonValue;
+
+    BpmnGatewayType(String jsonValue) {
+        this.jsonValue = jsonValue;
+    }
+
+    @JsonValue
+    public String getJsonValue() {
+        return jsonValue;
+    }
 }
