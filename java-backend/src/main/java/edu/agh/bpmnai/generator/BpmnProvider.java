2643d29824372da0ccfa2bd5a54e4b34267fb50c
Mateusz <mateusz.laczny@interia.pl>
2023-09-22 09:42:46 +0200

    feat: Further adjustments of prompts

--- java-backend/src/main/java/edu/agh/bpmnai/generator/BpmnProvider.java
+++ java-backend/src/main/java/edu/agh/bpmnai/generator/BpmnProvider.java
@@ -7,6 +7,8 @@ import edu.agh.bpmnai.generator.openai.model.ChatMessage;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class BpmnProvider {
@@ -15,35 +17,32 @@ public class BpmnProvider {
 
     public BpmnFile provideForTextPrompt(TextPrompt prompt) {
         BpmnModel bpmnModel = new BpmnModel();
+        Pattern p = Pattern.compile("\"\"\"(.*?)\"\"\"");
 
         ChatConversation chatConversation = OpenAIChatConversation.emptyConversationWith(OpenAI.OpenAIModel.GPT_3_5_TURBO_16K, temperature);
         chatConversation.addMessages(List.of(
-                ChatMessage.systemMessage("""
-                        Create BPMN models based on the business process description provided by the user. Follow the provided instructions."""),
-                ChatMessage.systemMessage("Step 1 - The user will provide the process description in triple quotes (\"\"\"). First, extend the process description provided by the user - think about all relevant specifics and details. Focus on the happy path in this step. Enclose all your work for this step within triple quotes (\"\"\")."),
-                ChatMessage.userMessage("\"\"\"" + prompt.content() + "\"\"\"")
+                ChatMessage.systemMessage("You will be provided a business process description. First work out your own business process description based on the one provided by the user. Think about all relevant specifics and details. Focus on the happy path in this step. Enclose all your work for this step within triple quotes (\"\"\""),
+                ChatMessage.userMessage(prompt.content())
         ));
         chatConversation.carryOutConversation(bpmnModel);
 
-        chatConversation.addMessage(ChatMessage.systemMessage("Step 2 - Think about the possible problems that could arise in the process. When you are done, modify the description you provided accordingly. Enclose all your work for this step within triple quotes (\"\"\")."));
+        ChatMessage modelResponse = chatConversation.getLastMessage();
+        Matcher m = p.matcher(modelResponse.content());
+
+        chatConversation.addMessage(ChatMessage.systemMessage("Think about the possible problems that could arise in the described process. When you are done, modify the description you provided accordingly. Enclose all your work for this step within triple quotes (\"\"\""));
+        chatConversation.addMessage(ChatMessage.userMessage("Users description: \"\"\"" + prompt.content() + "\"\"\"\n" +
+                "Your extended description: \n\n\n" + m.group(1) + "\"\"\"\n"));
         chatConversation.carryOutConversation(bpmnModel);
 
         chatConversation.addMessage(ChatMessage.systemMessage("""
-                Step 3 - Create the model based on your description by calling the provided functions. When creating the model use only the provided functions. Follow BPMN best practices:
-                - Final states with different meaning should be modeled as separate and described.
+                Create the model based on your description by calling the provided functions. When creating and manipulating the model only use the provided functions, do not provide the model source in your responses. Follow BPMN best practices:
+                - Different outcomes of the process should be modelled with different end states.
+                - End states with different meaning should be modeled as separate and described.
                 - Two end events in the same process should not have the same name.
                 - Don’t use XOR gateways to merge alternative paths.
                 - Don’t use gateway join into None end event.
                 - A sequence flow may not connect to another sequence flow, only to an activity, gateway or event.
                 """));
-        chatConversation.carryOutConversation(bpmnModel);
-
-        chatConversation.addMessage(ChatMessage.systemMessage("""
-                Step 4 - Fix the errors that could be introduced in the model. Use only the provided functions. Examples of errors:
-                - A gateway should have one incoming sequence flow, and two or more outgoing sequence flows.
-                - Elements connected through a gateway should not also be connected directly.
-                - Redundant paths between elements
-                - Gateway without a name"""));
         chatConversation.carryOutConversation(bpmnModel);
 
         return BpmnFile.fromModel(bpmnModel);
