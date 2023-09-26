b0da8736546e69bb7342a3405efebc282ee477d5
Mateusz <mateusz.laczny@interia.pl>
2023-09-26 09:48:28 +0200

    refactor: Removed unnecessary class

--- java-backend/src/main/java/edu/agh/bpmnai/generator/openai/model/FunctionCallExecutor.java
+++
@@ -1,9 +0,0 @@
-package edu.agh.bpmnai.generator.openai.model;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
-import java.util.Optional;
-
-public interface FunctionCallExecutor {
-    Optional<ChatMessage> executeFunction(String name, JsonNode parameters);
-}
