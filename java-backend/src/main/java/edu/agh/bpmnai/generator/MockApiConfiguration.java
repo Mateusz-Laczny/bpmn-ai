9c13c070943271c87f082ef1d68e77cbb99ce680
Mateusz <mateusz.laczny@interia.pl>
2023-10-18 13:23:04 +0200

    fix: Added a missing bean to the MockApiConfiguration

--- java-backend/src/main/java/edu/agh/bpmnai/generator/MockApiConfiguration.java
+++ java-backend/src/main/java/edu/agh/bpmnai/generator/MockApiConfiguration.java
@@ -2,9 +2,11 @@ package edu.agh.bpmnai.generator;
 
 import edu.agh.bpmnai.generator.bpmn.BpmnProvider;
 import edu.agh.bpmnai.generator.bpmn.MockBpmnProvider;
+import edu.agh.bpmnai.generator.bpmn.layouting.BpmnSemanticLayouting;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @Profile("mock-api")
@@ -12,5 +14,15 @@ public class MockApiConfiguration {
     @Bean
     public BpmnProvider bpmnProvider() {
         return new MockBpmnProvider();
+    }
+
+    @Bean
+    BpmnSemanticLayouting bpmnSemanticLayouting() {
+        return new BpmnSemanticLayouting();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
