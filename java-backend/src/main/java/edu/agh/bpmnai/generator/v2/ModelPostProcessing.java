94aea1431da1d213cc349154d2a8d5f46d792f9a
Mateusz <mateusz.laczny@interia.pl>
2024-05-01 10:35:58 +0200

    Add debug logs to ModelPostProcessing

--- java-backend/src/main/java/edu/agh/bpmnai/generator/v2/ModelPostProcessing.java
+++ java-backend/src/main/java/edu/agh/bpmnai/generator/v2/ModelPostProcessing.java
@@ -1,12 +1,13 @@
 package edu.agh.bpmnai.generator.v2;
 
 import edu.agh.bpmnai.generator.bpmn.BpmnManagedReference;
-import edu.agh.bpmnai.generator.bpmn.model.BpmnElementType;
 import edu.agh.bpmnai.generator.bpmn.model.BpmnModel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+
+import static edu.agh.bpmnai.generator.bpmn.model.BpmnElementType.GATEWAY;
 
 @Service
 @Slf4j
@@ -14,9 +15,10 @@ public class ModelPostProcessing {
 
     public void apply(BpmnManagedReference modelReference) {
         BpmnModel model = modelReference.getCurrentValue();
-        for (String gatewayId : model.findElementsOfType(BpmnElementType.GATEWAY)) {
+        for (String gatewayId : model.findElementsOfType(GATEWAY)) {
             Set<String> gatewaySuccessors = model.findSuccessors(gatewayId);
             if (gatewaySuccessors.size() == 1) {
+                log.debug("Gateway '{}' has only one successor and will be cut out from the model", gatewayId);
                 String gatewaySuccessorId = gatewaySuccessors.iterator().next();
                 for (String predecessorId : model.findPredecessors(gatewayId)) {
                     model.addUnlabelledSequenceFlow(predecessorId, gatewaySuccessorId);
