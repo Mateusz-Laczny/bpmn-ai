efe63c137c1ea5c0115db714cde37f50ce7be976
Mateusz <mateusz.laczny@interia.pl>
2024-05-01 10:35:35 +0200

    Change description of activityName parameter in Activity to make sure that the model generates unique activity names

--- java-backend/src/main/java/edu/agh/bpmnai/generator/v2/functions/parameter/Activity.java
+++ java-backend/src/main/java/edu/agh/bpmnai/generator/v2/functions/parameter/Activity.java
@@ -1,9 +1,9 @@
 package edu.agh.bpmnai.generator.v2.functions.parameter;
 
 public record Activity(
-        @Description("Name of the activity")
+        @Description("Name of the activity. Must be unique across the diagram.")
         String activityName,
-        @Description("Whether this activity marks the end of the process")
+        @Description("Whether this activity marks the end of the process.")
         boolean isProcessEnd
 ) {
 }
