396f97c4da95fd0c64ab5be46d067f4df6ffe32e
Mateusz <mateusz.laczny@interia.pl>
2024-05-10 14:58:18 +0200

    Remove unnecessary abstraction from exporting Bpmn to String representation

--- java-backend/src/main/java/edu/agh/bpmnai/generator/bpmn/BpmnToStringExporter.java
+++ java-backend/src/main/java/edu/agh/bpmnai/generator/bpmn/BpmnToStringExporter.java
@@ -1,26 +1,46 @@
 package edu.agh.bpmnai.generator.bpmn;
 
 import edu.agh.bpmnai.generator.bpmn.model.BpmnModel;
-import edu.agh.bpmnai.generator.v2.Graph;
-import edu.agh.bpmnai.generator.v2.GraphToStringExporter;
-import org.springframework.beans.factory.annotation.Autowired;
+import edu.agh.bpmnai.generator.bpmn.model.DirectedEdge;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class BpmnToStringExporter {
 
-    private final BpmnToGraphExporter bpmnToGraphExporter;
-
-    private final GraphToStringExporter graphToStringExporter;
-
-    @Autowired
-    public BpmnToStringExporter(BpmnToGraphExporter bpmnToGraphExporter, GraphToStringExporter graphToStringExporter) {
-        this.bpmnToGraphExporter = bpmnToGraphExporter;
-        this.graphToStringExporter = graphToStringExporter;
-    }
-
     public String export(BpmnModel model) {
-        Graph modelGraph = bpmnToGraphExporter.export(model);
-        return graphToStringExporter.export(modelGraph);
+        var nodeListBuilder = new StringBuilder("Nodes:\n");
+        var edgeListBuilder = new StringBuilder("Edges:\n");
+
+        Set<String> flowNodes = model.getFlowNodes();
+        if (!flowNodes.isEmpty()) {
+            for (String node : flowNodes) {
+                nodeListBuilder.append(model.getHumanReadableId(node).orElseThrow().asString()).append(",\n");
+            }
+        }
+
+        // Remove trailing newline
+        nodeListBuilder.deleteCharAt(nodeListBuilder.length() - 1);
+
+        Set<DirectedEdge> sequenceFlows = model.getSequenceFlows();
+        if (!sequenceFlows.isEmpty()) {
+            for (DirectedEdge sequenceFlow : sequenceFlows) {
+                edgeListBuilder.append('(')
+                               .append(model.getHumanReadableId(sequenceFlow.sourceId())
+                                            .orElseThrow()
+                                            .asString())
+                               .append(')')
+                               .append(" -> ")
+                               .append('(')
+                               .append(model.getHumanReadableId(sequenceFlow.targetId()).orElseThrow().asString())
+                               .append("),\n");
+            }
+        }
+
+        // Remove trailing newline
+        edgeListBuilder.deleteCharAt(nodeListBuilder.length() - 1);
+
+        return nodeListBuilder + "\n" + edgeListBuilder;
     }
 }
