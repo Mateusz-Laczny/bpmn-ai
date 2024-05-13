2435e98e0d142d70df7d0dfbf77f51cad0b01177
Mateusz <mateusz.laczny@interia.pl>
2024-05-13 14:09:08 +0200

    Update description and name of RespondWithoutModifyingDiagramFunction

--- java-backend/src/main/java/edu/agh/bpmnai/generator/v2/functions/RespondWithoutModifyingDiagramFunction.java
+++ java-backend/src/main/java/edu/agh/bpmnai/generator/v2/functions/RespondWithoutModifyingDiagramFunction.java
@@ -5,11 +5,11 @@ import edu.agh.bpmnai.generator.v2.functions.parameter.RespondWithoutModifyingDi
 import static edu.agh.bpmnai.generator.openai.OpenAIFunctionParametersSchemaFactory.getSchemaForParametersDto;
 
 public class RespondWithoutModifyingDiagramFunction {
-    public static final String FUNCTION_NAME = "respond_without_modifying_model";
+    public static final String FUNCTION_NAME = "user_request_does_not_require_modifying_the_diagram";
 
-    public static final ChatFunctionDto FUNCTION_DTO = ChatFunctionDto.builder()
-            .name(FUNCTION_NAME)
-            .description("Sends a message to the user, and stop without modifying the diagram. Use only if the user's request does not require modifying the diagram")
-            .parameters(getSchemaForParametersDto(RespondWithoutModifyingDiagramParametersDto.class))
-            .build();
+    public static final ChatFunctionDto FUNCTION_DTO = ChatFunctionDto.builder().name(FUNCTION_NAME).description(
+            "Sends a message to the user, and stop " + "without modifying the diagram. Use "
+            + "only if the user's request does not " + "require modifying the diagram, in "
+            + "other cases call the second provided" + " function.").parameters(getSchemaForParametersDto(
+            RespondWithoutModifyingDiagramParametersDto.class)).build();
 }
