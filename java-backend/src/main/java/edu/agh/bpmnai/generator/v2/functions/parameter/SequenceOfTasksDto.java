9d5f20f43da6e8991b87fb43b9af7704fe21b7d4
Mateusz <mateusz.laczny@interia.pl>
2024-05-20 19:31:19 +0200

    Update parameters descriptions for sequence of tasks subprocess function

--- java-backend/src/main/java/edu/agh/bpmnai/generator/v2/functions/parameter/SequenceOfTasksDto.java
+++ java-backend/src/main/java/edu/agh/bpmnai/generator/v2/functions/parameter/SequenceOfTasksDto.java
@@ -9,11 +9,11 @@ public record SequenceOfTasksDto(
                      + "be executed in this order, so I will add them as a sequence\"")
         String reasoning,
         @Description("Diagram node after which the subprocess will be inserted.  "
-                     + "Must be an element name that exists in the diagram. Must have exactly 0 or 1 successors.")
+                     + "Must be a node id which exists in the diagram. Must have exactly 0 or 1 successors.")
         String insertionPoint,
         @Description("Tasks which will be added to the diagram, in the verb+object naming convention. Each "
                      + "task will be connected to the next task via a sequence flow. Last task will be "
-                     + "connected to the current successor of the insertion point node.")
+                     + "connected to the current successor of the insertion point node, if it exists.")
         List<String> tasksInSequence
 ) {
 }
